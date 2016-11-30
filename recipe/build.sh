06f96533609586cec354ff65661489ebf61e8b76
Jonathan Helmus <jjhelmus@gmail.com>
2016-11-30 09:40:35 -0600

    Build from source on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,11 +11,19 @@ if [ `uname` == Darwin ]; then
 fi
 
 if [ `uname` == Linux ]; then
-    if [ "$PY_VER" == "2.7" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-${PKG_VERSION}-cp27-none-linux_x86_64.whl
-    elif [ "$PY_VER" == "3.4" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-${PKG_VERSION}-cp34-cp34m-linux_x86_64.whl
-    elif [ "$PY_VER" == "3.5" ]; then
-        pip install --no-deps https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-${PKG_VERSION}-cp35-cp35m-linux_x86_64.whl
-    fi
+    # Build tensorflow from source using cmake
+    rm -rf build
+    mkdir -p build
+    cd build
+
+    # The search path must be set so that zlib.h can be found
+    export CFLAGS="${CFLAGS} -I${PREFIX}/include"
+    # The rt library must be included during linking as the clock_ functions
+    # are not part of the main glibc library in older versions (prior to 2.17)
+    cmake \
+        -DCMAKE_EXE_LINKER_FLAGS=-lrt \
+        -DCMAKE_BUILD_TYPE=Release ../tensorflow/contrib/cmake
+    make -j8 tf_python_build_pip_package
+
+    pip install --no-deps ./tf_python/dist/*.whl
 fi
