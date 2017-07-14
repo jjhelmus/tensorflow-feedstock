2147197b04e1993159a7d19d64029a999c455598
Jonathan Helmus <jjhelmus@gmail.com>
2017-07-14 11:15:26 -0500

    Update to tensorflow 1.2.1, build from source
    * Update tensorflow to version 1.2.1
    * Build tensorflow from source using bazel
    
    Build is currently limited to a single Python version on Linux to limit CI
    time.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-# install using pip from the whl files on PyPI
+set -ex
 
-if [ `uname` == Darwin ]; then
-    if [ "$PY_VER" == "2.7" ]; then
-        WHL_FILE=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-${PKG_VERSION}-py2-none-any.whl
-    else
-        WHL_FILE=https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-${PKG_VERSION}-py3-none-any.whl
-    fi
-fi
+mkdir -p ./bazel_output_base
+export BAZEL_OPTS="--batch --output_base=./bazel_output_base"
 
-if [ `uname` == Linux ]; then
-    if [ "$PY_VER" == "2.7" ]; then
-        WHL_FILE=https://pypi.org/packages/cp27/t/tensorflow/tensorflow-${PKG_VERSION}-cp27-cp27mu-manylinux1_x86_64.whl
-    elif [ "$PY_VER" == "3.4" ]; then
-        WHL_FILE=https://pypi.org/packages/cp34/t/tensorflow/tensorflow-${PKG_VERSION}-cp34-cp34m-manylinux1_x86_64.whl
-    elif [ "$PY_VER" == "3.5" ]; then
-        WHL_FILE=https://pypi.org/packages/cp35/t/tensorflow/tensorflow-${PKG_VERSION}-cp35-cp35m-manylinux1_x86_64.whl
-    elif [ "$PY_VER" == "3.6" ]; then
-        WHL_FILE=https://pypi.org/packages/cp36/t/tensorflow/tensorflow-${PKG_VERSION}-cp36-cp36m-manylinux1_x86_64.whl
-    fi
-fi
+# Compile tensorflow from source
+export PYTHON_BIN_PATH=${PYTHON}
+export PYTHON_LIB_PATH=${SP_DIR}
+export CC_OPT_FLAGS="-march=nocona"
+# disable jemmloc (needs MADV_HUGEPAGE macro which is not in glib <= 2.12)
+export TF_NEED_JEMALLOC=0
+export TF_NEED_GCP=0
+export TF_NEED_HDFS=0
+export TF_ENABLE_XLA=0
+export TF_NEED_OPENCL=0
+export TF_NEED_CUDA=0
+export TF_NEED_MKL=0
+export TF_NEED_VERBS=0
+./configure
 
-pip install --no-deps $WHL_FILE
+# build using bazel
+bazel ${BAZEL_OPTS} build --config=opt //tensorflow/tools/pip_package:build_pip_package
+
+# build a whl file
+mkdir -p $SRC_DIR/tensorflow_pkg
+bazel-bin/tensorflow/tools/pip_package/build_pip_package $SRC_DIR/tensorflow_pkg
+
+# install using pip from the whl file
+pip install --no-deps $SRC_DIR/tensorflow_pkg/*.whl
